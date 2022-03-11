f62d0fbdd022b0641e35617a8e9731df53a9c39f
unknown <fedexmorlan@hotmail.com>
2022-03-11 12:32:02 -0300

    primer commit

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+// service worker!
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App subject="Clarice"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
