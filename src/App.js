9e084db5d74420c9a95b95415721d84b39ddef9a
unknown <fedexmorlan@hotmail.com>
2022-03-11 17:17:54 -0300

    agregar segunda parte de guía

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from "./components/FilterButton";
+
 
 function App(props) {
-  const subject = props.subject;
+  const taskList = props.tasks?.map(task => (
+    <Todo id={task.id} 
+    name={task.name} 
+    completed={task.completed}
+    key={task.id} />
+    ));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola, {subject} !
-        </p>  
-        
-      </header>
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form />
+      <div className="filters btn-group stack-exception">
+        {/* aria-pressed indica al screen reader si está presionado */}
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">
+        3 tasks remaining
+      </h2>
+      {/* https://www.scottohara.me/blog/2019/01/12/lists-and-safari.html */}
+      <ul
+        role="list"
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+      </ul>
     </div>
   );
 }
+
 
 export default App;
