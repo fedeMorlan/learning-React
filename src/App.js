4f8b91f9a2fa8651ca93b25d873c5d38d6a4528d
unknown <fedexmorlan@hotmail.com>
2022-03-12 22:55:45 -0300

    agregar siguiente parte de tutorial react

--- src/App.js
+++ src/App.js
@@ -2,27 +2,112 @@ import React from "react";
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
+/* Nota: definimos objetos constantes fuera de la función App
+porque sino serían recalculados cada vez que se re-renderice el componente */
+
+// objeto que usaremos para aplicar comportamientos a los filtros
+const FILTER_MAP = {
+  // para el filtro de "todos", nos devuelve todos
+  All: () => true, 
+  // para el filtro de "activos", nos devuelve los que no estén completados
+  Active: task => !task.completed,
+  // para el filtro de "completados", nos devuelve los que están completados
+  Completed: task => task.completed
+}
+
+// el método .keys recolecta un arreglo de FILTER_NAMES del objeto FILTER_MAP
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 function App(props) {
-  const taskList = props.tasks?.map(task => (
-    <Todo id={task.id} 
-    name={task.name} 
-    completed={task.completed}
-    key={task.id} />
-    ));
+  
+  function editTask(id, newName){
+    const editedTaskList = tasks.map(task => {
+      if (id === task.id){
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
+
+  // elimina una tarea según su id
+  function deleteTask(id) {
+    // aplica un filtro en los objetos, eliminando la que coincida con el id deseado
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
+
+  /* recibe un nombre y crea una tarea con ese nombre */
+  function addTask(name) {
+    const newTask = { id: "todo-" + nanoid(), name: name, completed:false };
+    setTasks([...tasks, newTask]);
+  }
+
+  // definir estado para manipular las tareas de la app
+  const [tasks, setTasks] = useState(props.tasks);
+
+  // definir filtros para manipular los filtros de tareas (todos, completado, incompleto)
+  const [filter, setFilter] = useState('All');
+
+  function toggleTaskCompleted(id) {
+    // mapea todo el arreglo de tareas, modificando (copia y reasignacion) sólo 1 de ellas
+    const updatedTasks = tasks.map(task =>{
+      //si esta task tiene el mismo ID que la editada
+      if (id === task.id) {
+        // usar object spread para hacer un nuevo objeto
+        // cuyo prop 'completed' se haya invertido
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+  // ya nos guardamos props.tasks en la variable "tasks"
+  const taskList = tasks
+    // se accede al valor en FILTER_MAP que corresponde a la clave del estado de filter
+    // cuando el filtro es "All", .filter(FILTER_MAP[filter]) evalúa a () => true
+    .filter(FILTER_MAP[filter])
+    .map(task => (
+      <Todo 
+        id={task.id} 
+        name={task.name} 
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask} 
+        editTask={editTask}
+      />
+    )
+  );
+
+  // constante para mapear sobre el arreglo de nombres y retornar un componente FilterButton
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+  
+    // conteo de cantidad de tareas (1 "tarea" o "tareas")
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      {/* addTask es una funcion que se manda como prop: callback prop */}
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        {/* aria-pressed indica al screen reader si está presionado */}
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
       <h2 id="list-heading">
-        3 tasks remaining
+        {headingText}
       </h2>
       {/* https://www.scottohara.me/blog/2019/01/12/lists-and-safari.html */}
       <ul
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,38 @@
-import React from "react";
+/* importar la funcion HOOK para usar el estado interno de un componente */
+import React, {useState} from "react";
+
 
 function Form(props){
+    /* useState crea una pieza de estado para un componente;
+    recibe un parámetro que determina el valor inicial de ese estado;
+    retorna el estado Y una función que se puede usar para actualizar ese estado */
+    /* crear un estado "name" (y una funcion para actualizarlo) */
+    const [name, setName] = useState("usar hooks!");
+
+    /* el prop addTask que recibe este componente es una función que espera como parametro el nombre de la tarea */
+    function handleSubmit(e){
+        e.preventDefault();
+        // usando regex para chequear que el string no esté vacío:
+        // ^ = que no empiece con; \s = espacio; + = uno o más caracteres iguales; $ = que no termine con
+        if (name !== /^\s+$/){
+            props.addTask(name);
+        }
+        // una vez que ya agregamos el nuevo elemento con su nombre, vaciamos el campo de input
+        setName("");
+    }
+
+    function handleChange(e){
+        /* e representa en react al onChange (?): un console.log(e) retorna un SyntheticBaseEvent Object
+        cuyo _reactName es "onChange", y type es "change" 
+
+        e.target represena el elemento que disparó el evento change (el elemento <input> nuevo!);
+        value es el texto dentro de ese elemento */
+        setName(e.target.value);
+    }
+
     return(
-        <form>
+        /* ejecutar la función deseada */
+        <form onSubmit={handleSubmit}>
             <h2 className='label-wrapper'>
                 <label htmlFor="new-todo-input" className="label__lg">
                     What needs to be done?
@@ -14,6 +44,9 @@ function Form(props){
                 className="input input__lg"
                 name="text"
                 autoComplete="off"
+                /* tomar valores del estado de este componente */
+                value={name}
+                onChange={handleChange}
             />
             <button type="submit" className="btn btn__primary btn__lg">
                 Add
